4d327c34cfb1e35f0c79eccb0757557291c33bbf
Somesh Musunuri <somesh.musunuri@team.telstra.com>
2025-12-18 23:57:33 +0530

    feat: update tests for day 1 solutions

--- day-1/day-1.test.ts
+++ day-1/day-1.test.ts
@@ -4,9 +4,14 @@ import { run as part2 } from "./part-2.ts";
 import { SIMPLE_INPUT, MAIN_INPUT } from "../utils/constants.ts";
 
 
-test.only('day-1 part 1, with simple input', async () => {
+test('day-1 part 1, with simple input', async () => {
     const result = await part1(SIMPLE_INPUT)
     expect(result).toBe(3)
+})
+
+test('day-1 part 1, with main input', async () => {
+    const result = await part1(MAIN_INPUT)
+    expect(result).toBe(1066)
 })
 
 test('day-1 part 2, with simple input', async () => {
